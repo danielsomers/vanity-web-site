f500ba8ac74047b7b4370e931f628111a18b8b6f
root <root@web.(none)>
2013-02-22 17:44:32 +0000

    Now a rails project.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+VanityWebSite::Application.config.secret_token = '8c487d3ae8c1f6a816b8e7b503e05f23f56b7ddaa44782db547a0304006ded7464c9f30e4e70877aaa6432a1d9c6251571b51c05745c746693f399e3f7d7f6f4'
